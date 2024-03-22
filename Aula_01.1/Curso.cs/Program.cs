51eb94a362938626cd0126531e0b8fefc14af2bc
PedroA07 <pedro.andrade.pereira@hotmail.com.br>
2024-03-22 11:51:26 -0300

    Create paste Aula_01.1

---
+++ Aula_01.1/Curso.cs/Program.cs
@@ -0,0 +1,18 @@
+﻿//Biblioteca:
+
+//A biblioteca System que trás as funcionalidades básicas de entrada e saída.
+using System;
+
+namespace Curso.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Printa na tela o texto escrito.
+            Console.WriteLine("Olá, mundo!");
+            //Cria uma pausa após o texto.
+            Console.ReadLine();
+        }
+    }
+}
