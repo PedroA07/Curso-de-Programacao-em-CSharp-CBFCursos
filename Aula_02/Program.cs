b092384c8a328fa8a883526d31d30b567777ff25
PedroA07 <pedro.andrade.pereira@hotmail.com.br>
2024-04-03 00:50:40 -0300

    Update

---
+++ Aula_02/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Para gerar o arquivo já configurado neste padrão antigo, basta digitar o seguinte comando no console:
+//  dotnet new console --use-program-main
+
+using System;
+
+namespace Aula_02{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Hello, World!");
+        }
+}
+}
